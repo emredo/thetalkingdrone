fb0749ec7e0daf463a9f4129e876a47e1a4e797f
Emre Çimen <hemre.cimen@caresept.com>
2025-05-06 12:44:58 +0300

    dev

--- src/config/llm_config.py
+++ src/config/llm_config.py
@@ -1,11 +1,16 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from src.constant.keys import GOOGLE_API_KEY
+from src.utils.logger import logger
 
 
 def get_llm_object():
-    return ChatGoogleGenerativeAI(
-        model="gemini-2.5-pro-exp-03-25",
-        temperature=0.2,
-        google_api_key=GOOGLE_API_KEY,
-    )
+    try:
+        return ChatGoogleGenerativeAI(
+            model="gemini-2.5-pro-exp-03-25",
+            temperature=0.2,
+            google_api_key=GOOGLE_API_KEY,
+        )
+    except Exception as e:
+        logger.error(f"Error getting LLM object: {e}")
+        raise Exception(f"Error getting LLM object: {e}")
