9cbd7d6654373177472a2d5099d77ae668155d8c
Emre Çimen <hemre.cimen@caresept.com>
2025-06-02 14:42:12 +0300

    change model

--- src/config/llm_config.py
+++ src/config/llm_config.py
@@ -7,7 +7,7 @@ from src.utils.logger import logger
 def get_llm_object():
     try:
         return ChatGoogleGenerativeAI(
-            model="gemini-2.5-pro-exp-03-25",
+            model="gemini-2.5-flash-preview-05-20",
             temperature=0.2,
             google_api_key=GOOGLE_API_KEY,
         )
